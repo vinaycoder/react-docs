dfe3ec90b37f64b7b30ec6b59e24bdf96f921670
Vinay kumar <vinaykr.bca@gmail.com>
2025-06-21 06:51:55 +0530

    Update YourContainerComponent.jsx

--- react-query/YourContainerComponent.jsx
+++ react-query/YourContainerComponent.jsx
@@ -1,4 +1,21 @@
+import { useApiOnPageLoad, PostApi } from '../services';
 const YourContainerComponent = () =>{
-    const { isLoading, error, startTimer, orderData, orderAem } = useApiOnPageLoad();
+    const { isLoading, error, startTimer, orderData, orderAem } = useApiOnPageLoad(); /// this is a get api call
+    // below is example of post api call
+     const submitFomr = async (payLoad) => {
+    try {
+      const [orderRes] = await Promise.allSettled([
+        // mutateAsync(payLoad),
+        PostApi(consentPayload),
+      ]);
+
+      const aemErrorMessage = orderRes?.value.error;
+      if (aemErrorMessage) {
+        return;
+      }
+
+    } catch (error) {
+    }
+  };
   
 }
